ea5b834c28899d6559f9383737373c78a5171ab9
alexn <naum1979alex@gmail.com>
2022-05-12 22:22:02 +0300

    Lesson 2 - First task

--- Lesson2/src/Lesson2/Numbers.java
+++ Lesson2/src/Lesson2/Numbers.java
@@ -2,14 +2,37 @@ package Lesson2;
 
 public class Numbers {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            //name = con.readLine("Please, enter your name ");
+        for (int i = 1; i <= 10; i++) {
             switch (i) {
                 case 1:
                     System.out.println("one");
                     break;
                 case 2:
                     System.out.println("two");
+                    break;
+                case 3:
+                    System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+                case 5:
+                    System.out.println("five");
+                    break;
+                case 6:
+                    System.out.println("six");
+                    break;
+                case 7:
+                    System.out.println("seven");
+                    break;
+                case 8:
+                    System.out.println("eight");
+                    break;
+                case 9:
+                    System.out.println("nine");
+                    break;
+                case 10:
+                    System.out.println("ten");
                     break;
             }
         }
