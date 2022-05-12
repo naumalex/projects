b8f57f174b02829d371fc05ef282ecdfb0e28ed9
alexn <naum1979alex@gmail.com>
2022-05-12 21:53:44 +0300

    Lesson 2 - First task

---
+++ Lesson2/src/Lesson2/Numbers.java
@@ -0,0 +1,17 @@
+package Lesson2;
+
+public class Numbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            //name = con.readLine("Please, enter your name ");
+            switch (i) {
+                case 1:
+                    System.out.println("one");
+                    break;
+                case 2:
+                    System.out.println("two");
+                    break;
+            }
+        }
+    }
+}
