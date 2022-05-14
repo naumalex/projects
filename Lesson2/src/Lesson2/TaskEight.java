fc27ee8406da5bbd8cbb029e80055f78762cb082
alexn <naum1979alex@gmail.com>
2022-05-14 20:21:57 +0300

    Task Eight

---
+++ Lesson2/src/Lesson2/TaskEight.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class TaskEight {
+    public static void main (String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (i%2 == 0)
+                System.out.printf("%d ", i);
+        }
+    }
+}
