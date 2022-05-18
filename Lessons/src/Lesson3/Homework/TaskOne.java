9f50fce757aa711396e88759f99dbb4d45cfe5c9
alexn <naum1979alex@gmail.com>
2022-05-18 09:36:30 +0300

    Task 4

--- Lessons/src/Lesson3/Homework/TaskOne.java
+++ Lessons/src/Lesson3/Homework/TaskOne.java
@@ -2,10 +2,9 @@ package Lesson3.Homework;
 
 public class TaskOne {
     public static void main (String[] args) {
-        int[] mas = new int[10];
-        mas[0] = 2;
-        for (int i = 1; i < 10; i++)
-            mas[i] = mas[i-1] + 2;
+        int[] mas = new int[11];
+        for (int i = 0; i < 11; i++)
+            mas[i] = 2 * i;
 
         System.out.println("Show the array as a row");
         for (int i: mas) {
